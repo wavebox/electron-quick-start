b3b7321e0af8e6a3a83aad89dc8e0c2799866764
Thomas Beverley <tom@locly.com>
2017-05-19 15:47:28 +0100

    Update to show assertion error. Retest with 1.7.1

--- main.js
+++ main.js
@@ -10,27 +10,21 @@ const url = require('url')
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
 let mainWindow
+let errorWindow
 
 function createWindow () {
-  // Create the browser window.
   mainWindow = new BrowserWindow({width: 800, height: 600})
-
-  // and load the index.html of the app.
-  mainWindow.loadURL(url.format({
-    pathname: path.join(__dirname, 'index.html'),
-    protocol: 'file:',
-    slashes: true
-  }))
-
-  // Open the DevTools.
-  // mainWindow.webContents.openDevTools()
-
-  // Emitted when the window is closed.
+  mainWindow.loadURL('http://www.pdf995.com/samples/pdf.pdf')
+  mainWindow.webContents.openDevTools()
   mainWindow.on('closed', function () {
-    // Dereference the window object, usually you would store windows
-    // in an array if your app supports multi windows, this is the time
-    // when you should delete the corresponding element.
     mainWindow = null
+  })
+
+  errorWindow = new BrowserWindow({width: 800, height: 600})
+  errorWindow.loadURL('https://1drv.ms/b/s!AtilKnwD6Yt6xSqGHCpSLlZF0ZUz')
+  errorWindow.webContents.openDevTools()
+  errorWindow.on('closed', function () {
+    errorWindow = null
   })
 }
 
