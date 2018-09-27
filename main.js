a28a3434e8e20e5a913c0ca17671357ba5bfb3ac
Thomas Beverley <tom@wavebox.io>
2018-09-27 14:24:47 +0100

    Example bug

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const electron = require('electron')
 // Module to control application life.
 const app = electron.app
+app.enableMixedSandbox()
 // Module to create native browser window.
 const BrowserWindow = electron.BrowserWindow
 
